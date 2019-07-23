6988bbc883dc1de221ecb86bac5baddab73fed5b
puntonim <puntonim@gmail.com>
2019-07-23 19:52:46 +0200

    Updates

---
+++ precache-manifest.12a6358f4fe377509971b920046f3cbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31627e4bd62949d0a283af3d4cd36814",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59f0db14bcb77deb459a",
+    "url": "./static/css/2.4531a876.chunk.css"
+  },
+  {
+    "revision": "f79166726a34c910dd0a",
+    "url": "./static/css/main.43bd1275.chunk.css"
+  },
+  {
+    "revision": "59f0db14bcb77deb459a",
+    "url": "./static/js/2.730a5f5b.chunk.js"
+  },
+  {
+    "revision": "f79166726a34c910dd0a",
+    "url": "./static/js/main.f9fab9fd.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
