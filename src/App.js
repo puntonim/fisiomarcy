dae5f877579d46dca5049ba8162d065867abcdff
puntonim <puntonim@gmail.com>
2019-07-23 18:01:58 +0200

    NEW Potenza and Cardio pages connected to nav menu

--- src/App.js
+++ src/App.js
@@ -1,53 +1,59 @@
 import React from 'react'
-import { Layout, Button, Menu, Breadcrumb } from 'antd'
-import { Typography } from 'antd'
-
+import { Layout, Menu } from 'antd'
+import Potenza from './Potenza'
+import Cardio from './Cardio'
 const { Header, Footer, Content } = Layout
-const { Title, Paragraph, Text } = Typography
 
-function App() {
-    return (
-        <Layout>
-            <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-                <Menu
-                    theme='dark'
-                    mode='horizontal'
-                    defaultSelectedKeys={['2']}
-                    style={{ lineHeight: '64px' }}
-                >
-                    <Menu.Item key='1'>Potenza</Menu.Item>
-                    <Menu.Item key='2'>Cardio</Menu.Item>
-                </Menu>
-            </Header>
-            <Content style={{ padding: '0 50px', marginTop: 64 }}>
-                <div
-                    style={{ background: '#fff', padding: 24, minHeight: 380 }}
-                >
-                    <Typography>
-                        <Title>Hello World!</Title>
-                        <Paragraph>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Vestibulum non nisi a lorem efficitur egestas.
-                            Morbi dolor massa, imperdiet ut purus et, ultrices
-                            egestas quam. Mauris velit purus, venenatis a sem
-                            vitae, mattis maximus libero. Maecenas cursus erat
-                            quis turpis venenatis, sit amet varius mi ultricies.
-                            Etiam sit amet volutpat sem, sed pellentesque
-                            lectus. Morbi quis lacus vitae libero dictum mattis.
-                            Maecenas placerat faucibus eros ac accumsan. Morbi
-                            non risus et sapien porttitor facilisis id auctor
-                            sapien. Aenean sed lacus ligula. Quisque tempus
-                            tempus velit, non tincidunt turpis congue ac.
-                            Quisque condimentum porta eleifend. Vivamus a velit
-                            leo. Quisque eleifend quis mauris non ullamcorper.
-                        </Paragraph>
-                        <Button type='primary'>Next</Button>
-                    </Typography>
-                </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>Fisio Marcy</Footer>
-        </Layout>
-    )
+export default class App extends React.Component {
+    defaultMenuItem = '1'
+    state = { selectedItem: this.defaultMenuItem }
+
+    render() {
+        return (
+            <Layout>
+                <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+                    <Menu
+                        theme='dark'
+                        mode='horizontal'
+                        defaultSelectedKeys={[this.defaultMenuItem]}
+                        style={{ lineHeight: '64px' }}
+                    >
+                        <Menu.Item
+                            key='1'
+                            onClick={this.onItemClick.bind(this, '1')}
+                        >
+                            Potenza
+                        </Menu.Item>
+                        <Menu.Item
+                            key='2'
+                            onClick={this.onItemClick.bind(this, '2')}
+                        >
+                            Cardio
+                        </Menu.Item>
+                    </Menu>
+                </Header>
+                <Content style={{ padding: '0 50px', marginTop: 64 }}>
+                    <div
+                        style={{
+                            background: '#fff',
+                            padding: 24,
+                            minHeight: 380,
+                        }}
+                    >
+                        {this.state.selectedItem !== this.defaultMenuItem ? (
+                            <Cardio />
+                        ) : (
+                            <Potenza />
+                        )}
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Fisio Marcy</Footer>
+            </Layout>
+        )
+    }
+
+    onItemClick(i) {
+        console.log(i)
+        this.setState({ ...this.state, selectedItem: i })
+    }
 }
-
-export default App
---
+++ src/Cardio.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Typography } from 'antd'
+
+const { Title, Paragraph, Text } = Typography
+
+export default function Cardio() {
+    return (
+        <Typography>
+            <Title>Cardio</Title>
+            <Paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum non nisi a lorem efficitur egestas. Morbi dolor
+                massa, imperdiet ut purus et, ultrices egestas quam. Mauris
+                velit purus, venenatis a sem vitae, mattis maximus libero.
+                Maecenas cursus erat quis turpis venenatis, sit amet varius mi
+                ultricies. Etiam sit amet volutpat sem, sed pellentesque lectus.
+                Morbi quis lacus vitae libero dictum mattis. Maecenas placerat
+                faucibus eros ac accumsan. Morbi non risus et sapien porttitor
+                facilisis id auctor sapien. Aenean sed lacus ligula. Quisque
+                tempus tempus velit, non tincidunt turpis congue ac. Quisque
+                condimentum porta eleifend. Vivamus a velit leo. Quisque
+                eleifend quis mauris non ullamcorper.
+            </Paragraph>
+        </Typography>
+    )
+}
