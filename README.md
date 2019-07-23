fadd8d235560ebb3921b85ac47e591853ba87b4e
puntonim <puntonim@gmail.com>
2019-07-23 17:40:35 +0200

    NEW Basic layout with navbar

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Fisio Marcy
+
+Pagina web per istruzioni sulle sessioni di fisioterapia.
+
 ## How to create a React project with Antd:
 ```bash
 $ npx create-react-app fisiomarcy
@@ -5,3 +9,16 @@ $ cd fisiomarcy
 $ npm install --save antd
 $ npm install babel-plugin-import --save-dev
 ```
+
+Then, to enable auto import of css files for Ant components:
+```bash
+$ git commit
+$ npm run eject
+```
+Add this code to the babel section in package.json:
+```json
+"plugins": [["import", { "libraryName": "antd", "style": "css" }]]
+```
+
+## Copyright
+Copyright 2019 puntonim (https://github.com/puntonim). No License.
--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import React from 'react'
-import { Button } from 'antd'
+import { Layout, Button, Menu, Breadcrumb } from 'antd'
+import { Typography } from 'antd'
+
+const { Header, Footer, Content } = Layout
+const { Title, Paragraph, Text } = Typography
 
 function App() {
     return (
-        <div>
-            Hello World!
-            <Button type='primary'>Button</Button>
-        </div>
+        <Layout>
+            <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+                <Menu
+                    theme='dark'
+                    mode='horizontal'
+                    defaultSelectedKeys={['2']}
+                    style={{ lineHeight: '64px' }}
+                >
+                    <Menu.Item key='1'>Potenza</Menu.Item>
+                    <Menu.Item key='2'>Cardio</Menu.Item>
+                </Menu>
+            </Header>
+            <Content style={{ padding: '0 50px', marginTop: 64 }}>
+                <div
+                    style={{ background: '#fff', padding: 24, minHeight: 380 }}
+                >
+                    <Typography>
+                        <Title>Hello World!</Title>
+                        <Paragraph>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Vestibulum non nisi a lorem efficitur egestas.
+                            Morbi dolor massa, imperdiet ut purus et, ultrices
+                            egestas quam. Mauris velit purus, venenatis a sem
+                            vitae, mattis maximus libero. Maecenas cursus erat
+                            quis turpis venenatis, sit amet varius mi ultricies.
+                            Etiam sit amet volutpat sem, sed pellentesque
+                            lectus. Morbi quis lacus vitae libero dictum mattis.
+                            Maecenas placerat faucibus eros ac accumsan. Morbi
+                            non risus et sapien porttitor facilisis id auctor
+                            sapien. Aenean sed lacus ligula. Quisque tempus
+                            tempus velit, non tincidunt turpis congue ac.
+                            Quisque condimentum porta eleifend. Vivamus a velit
+                            leo. Quisque eleifend quis mauris non ullamcorper.
+                        </Paragraph>
+                        <Button type='primary'>Next</Button>
+                    </Typography>
+                </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Fisio Marcy</Footer>
+        </Layout>
     )
 }
 
